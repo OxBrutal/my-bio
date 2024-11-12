6f6e0eb10a36a70ee4256a7d1742f3e97f48e695
Koi <61906304+OxBrutal@users.noreply.github.com>
2024-11-12 16:03:47 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -255,7 +255,7 @@ export default function Home() {
                     </div>
                     <div className="bg-animate group mb-1 w-full cursor-pointer rounded bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-900 text-sm text-zinc-400 shadow transition-all">
                           <a
-                            href="https://www.subs4unlock.id/VwyjeN"
+                            href="https://www.subs4unlock.id/oU2VyU"
                             target="_blank"
                             rel="noopener noreferrer nofollow"
                             className="w-full transition-all sm:hover:opacity-70"
