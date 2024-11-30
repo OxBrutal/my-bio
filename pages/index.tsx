0caf258812439d7fd00aa9afdbc4817c378459df
Koi <61906304+OxBrutal@users.noreply.github.com>
2024-11-30 16:34:43 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -255,7 +255,7 @@ export default function Home() {
                     </div>
                     <div className="bg-animate group mb-1 w-full cursor-pointer rounded bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-900 text-sm text-zinc-400 shadow transition-all">
                           <a
-                            href="https://www.subs4unlock.id/oU2VyU"
+                            href="https://www.subs4unlock.id/ATJvlo"
                             target="_blank"
                             rel="noopener noreferrer nofollow"
                             className="w-full transition-all sm:hover:opacity-70"
@@ -300,7 +300,7 @@ export default function Home() {
                        </div>
                       <div className="bg-animate group mb-1 w-full cursor-pointer rounded bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-900 text-sm text-zinc-400 shadow transition-all">
                           <a
-                            href="https://www.subs4unlock.id/3KlrCA"
+                            href="https://www.subs4unlock.id/Ly5it9"
                             target="_blank"
                             rel="noopener noreferrer nofollow"
                             className="w-full transition-all sm:hover:opacity-70"
